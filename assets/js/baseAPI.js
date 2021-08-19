0b653aad586196d3847b9dc01de96bba301c652d
evelyn <1634752696@qq.com>
2021-08-19 18:01:27 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,24 @@
 $.ajaxPrefilter(function(options) {
     // 统一拼接ajax的根路径
     options.url = ' http://api-breakingnews-web.itheima.net' + options.url
+
+
+    // 统一为有权限的接口设置请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    // 全局统一挂载
+    options.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空 token 
+            localStorage.removeItem('token')
+                // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
